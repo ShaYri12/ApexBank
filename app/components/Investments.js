1c68b2a757a24c27d9f702cd48d2db9933213808
ShaYri12 <sharjeelb60@gmail.com>
2024-06-17 01:40:23 +0500

    almost done

--- app/components/Investments.js
+++ app/components/Investments.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const Investments = () => {
   return (
     <Card className="p-6 bg-white dark:bg-zinc-800 rounded-lg shadow-md">
-      <CardHeader className="flex justify-between items-center mb-4">
-        <CardTitle className="text-lg font-semibold text-zinc-700 dark:text-zinc-200">Investimentos</CardTitle>
-        <button className="text-zinc-500 dark:text-zinc-400">+</button>
+      <CardHeader className="flex flex-row justify-between items-center">
+        <CardTitle className="text-left">Investimentos</CardTitle>
+        <Button
+            variant="ghost"
+            size="icon"
+            className="bg-white rounded-full border border-dotted border-[#D0D5DD]"
+          >
+            <img
+              width={"16px"}
+              height={"16px"}
+              src="/assets/plus.svg"
+              alt="plus"
+            />
+          </Button>
       </CardHeader>
       <CardContent className="flex flex-col lg:flex-row">
         <div className="flex-1 flex flex-col items-center lg:items-start lg:flex-row lg:space-x-4">
           <div className="w-24 h-24 mb-4 lg:mb-0">
-            <img src="https://placehold.co/100x100" alt="Donut Chart" className="w-full h-full" />
+            <img src="/assets/pie-chart.svg" alt="Donut Chart" className="w-full h-full" />
           </div>
           <div>
             <CardDescription className="text-zinc-500 dark:text-zinc-400">Balanço total</CardDescription>
@@ -22,7 +34,7 @@ const Investments = () => {
           </div>
         </div>
         <div className="flex-1">
-          <img src="https://placehold.co/400x200" alt="Line Chart" className="w-full h-full" />
+          <img src="/assets/line-bar-chart.svg" alt="Line Chart" className="w-full h-full" />
         </div>
       </CardContent>
       <CardContent className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
